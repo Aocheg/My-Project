b45b8b734ba915de52da81c1275f25920c498b22
Aocheg <oche8004@gmail.com>
2026-06-05 17:04:24 +0100

    updated work

---
+++ practice/pract.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello")
+}
+
+func Clap() {
+	fmt.Println("👏")
+}
+func Greet(name string) {
+	fmt.Println("Hello,", name)
+}
+func Add(a int, b int) int {
+	return a + b
+}
+func Double(number int) int {
+	return number * 2
+}
+
+// func IfStatement(age int) string {
+// 	age = 20
+// 	if age >= 18 {
+// 		return "come in"
+// 	}
+
+// }
+func main() {
+	age := 10
+	if age >= 18 {
+		fmt.Println("Come in")
+	} else {
+		println("Too young")
+	}
+	score := 70
+	if score >= 50 {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
+	for i := 0; i < 4; i++ {
+		fmt.Println("Go")
+	}
+	toys := [3]string{"car", "ball", "robot"}
+	fmt.Println(toys)
+	for i := 0; i < 3; i++ {
+		fmt.Println(toys[i])
+	}
+	fruits := [3]string{"apple", "banana", "mango"}
+	fmt.Println(fruits[1])
+	fruit := []string{"banana", "apple"}
+	fruit = append(fruit, "orange")
+	fmt.Println(fruit)
+	names1 := []string{"Mary", "John", "Peter"}
+	fmt.Println(names1[0])
+	SayHello()
+	Clap()
+	Clap()
+	Greet("Adanu!")
+	Greet("Gabriel!!")
+	result := Add(2, 4)
+	fmt.Println(result)
+	answer := Double(5)
+	fmt.Println(answer)
+
+}
